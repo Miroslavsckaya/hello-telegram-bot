ad11519aa7eadf2d20d4d3b7ab647d0348528c10
Stanislava M <stmiroslavsckaya@yandex.ru>
2022-02-02 17:01:16 +0300

    responds 'Hello' to the user's message

--- hello_bot.py
+++ hello_bot.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 
 def send_message(token, id, text):
@@ -11,3 +12,16 @@ def get_message(token, offset):
     return response.json()
 
 
+if len(sys.argv) != 2:
+    print('Usage: ./hello_bot.py token')
+    sys.exit(1)
+
+token = sys.argv[1]
+offset = -1
+
+while True:
+    message = get_message(token, offset)
+    if message['ok'] and message['result']:
+        chat_id = message['result'][0]['message']['chat']['id']
+        send_message(token, chat_id, 'Hello!')
+        offset = message['result'][0]['update_id'] + 1
