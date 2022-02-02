0f3f09bb25bff5d71cd11482e2fade6d59edc589
Stanislava M <stmiroslavsckaya@yandex.ru>
2022-02-02 14:36:58 +0300

    add get message function

--- hello_bot.py
+++ hello_bot.py
@@ -1,5 +1,13 @@
 import requests
 
+
 def send_message(token, id, text):
     response = requests.post(f'https://api.telegram.org/bot{token}/sendMessage', json={'chat_id': id, 'text': text})
     return response.status_code == 200
+
+
+def get_message(token, offset):
+    response = requests.get(f'https://api.telegram.org/bot{token}/getUpdates', params={'offset': offset,'timeout': 60})
+    return response.json()
+
+
