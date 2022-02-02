dbe1f48f90fa8eccfa0a091a73db388836c14e2b
Stanislava M <stmiroslavsckaya@yandex.ru>
2022-02-02 12:38:47 +0300

    add send message function

---
+++ hello_bot.py
@@ -0,0 +1,5 @@
+import requests
+
+def send_message(token, id, text):
+    response = requests.post(f'https://api.telegram.org/bot{token}/sendMessage', json={'chat_id': id, 'text': text})
+    return response.status_code == 200
